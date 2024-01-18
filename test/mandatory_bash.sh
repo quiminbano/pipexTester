9028a33691b4cc6ba5c7ca3631a4d11f418fa910
Carlos Orellana <corellan@c1r1p4.hive.fi>
2024-01-18 19:15:39 +0200

    trying to add some questions to the user

--- test/mandatory_bash.sh
+++ test/mandatory_bash.sh
@@ -8,24 +8,27 @@ yellow='\033[0;33m'
 brightyellow='\033[1;33m'
 pink='\033[38;5;161m'
 nocolor='\033[0m'
+brightnocolor='\033[1m'
 
 valgrindConst=200;
+options=("y" "n")
 pipexInstructions=("./pipex '' '' '' ''" \
 					"./pipex '' 'cat' 'cat' ''" \
 					"./pipex 'infile' 'cat' 'cat' ''" \
-					"./pipex '' 'cat' 'cat' 'outfile'" \
-					"./pipex 'infile' '' '' 'outfile'" \
-					"./pipex 'infile' 'cat' 'cat' 'outfile'" \
-					"./pipex 'infile' './testsegv' './testsegv' 'outfile'" \
-					"./pipex 'infile' './myfolder' 'cat' 'outfile'" \
-					"./pipex 'infile' 'cat' './myfolder' 'outfile'" \
-					"./pipex 'infile' '/bin/hello' '/bin/hello' 'outfile'" \
-					"./pipex 'infile' '/bin/echo hello world' '/bin/cat' outfile" \
-					"./pipex 'testsegv.c' 'cat' 'grep str\ =\ NULL' 'outfile'" \
-					"./pipex 'infile2' 'cat' 'awk -F \";\" \"{print \$1}\"' 'outfile'" \
-					"./pipex 'infile2' '\"l\"\"s\"' '\"l\"\"s\"\"\" \"\"\"\"\"-\"\"l\"\"a\"' 'outfile'" \
-					"./pipex 'infile2' '\a\b\c' '\"l\"\"s\"\"\"\ \"\"\"\"\"-\"\"l\"\"a\"' 'outfile'" \
-					"./pipex 'infile2' '\a\b\c\' '\"l\"\"s\"\"\"\ \"\"\"\"\"-\"\"l\"\"a\"' 'outfile'")
+					"./pipex '' 'cat' 'cat' 'outfile2'" \
+					"./pipex 'infile' '' '' 'outfile2'" \
+					"./pipex 'infile' 'cat' 'cat' 'outfile2'" \
+					"./pipex 'infile' '/bin/hello' '/bin/hello' 'outfile2'" \
+					"./pipex 'infile' '/bin/echo hello world' '/bin/cat' outfile2" \
+					"./pipex 'infile' 'sleep 5' 'echo hello world' 'outfile2'" \
+					"./pipex 'infile' './testsegv' './testsegv' 'outfile2'" \
+					"./pipex 'infile' './myfolder' 'cat' 'outfile2'" \
+					"./pipex 'infile' 'cat' './myfolder' 'outfile2'" \
+					"./pipex 'testsegv.c' 'cat' 'grep str\ =\ NULL' 'outfile2'" \
+					"./pipex 'infile2' 'cat' 'awk -F \";\" \"{print \$1}\"' 'outfile2'" \
+					"./pipex 'infile2' '\"l\"\"s\"' '\"i\"\"p\"\"\" \"\"\"\"\"-\"\"\"\"s\" \"\"\"l\"\"i\"\"nk\"' 'outfile2'" \
+					"./pipex 'infile2' '\a\b\c' '\"l\"\"s\"\"\"\ \"\"\"\"\"-\"\"l\"\"a\"' 'outfile2'" \
+					"./pipex 'infile2' '\a\b\c\' '\"l\"\"s\"\"\"\ \"\"\"\"\"-\"\"l\"\"a\"' 'outfile2'")
 
 shellInstructions=("< '' '' | '' > ''" \
 				"< '' cat | cat > ''" \
@@ -33,14 +36,15 @@ shellInstructions=("< '' '' | '' > ''" \
 				"< '' cat | cat > outfile" \
 				"< infile '' | '' > outfile" \
 				"< infile cat | cat > outfile" \
+				"< infile /bin/hello | /bin/hello > outfile" \
+				"< infile /bin/echo hello world | /bin/cat > outfile" \
+				"< infile sleep 5 | echo hello world > outfile" \
 				"< infile ./testsegv | ./testsegv > outfile" \
 				"< infile ./myfolder | cat > outfile" \
 				"< infile cat | ./myfolder > outfile" \
-				"< infile /bin/hello | /bin/hello > outfile" \
-				"< infile /bin/echo hello world | /bin/cat > outfile" \
 				"< testsegv.c cat | grep str\ =\ NULL > outfile" \
 				"< infile2 cat | awk -F \";\" '{print \$1}' > outfile" \
-				"< infile2 \"l\"\"s\" | \"l\"\"s\"\"\" \"\"\"\"\"-\"\"l\"\"a\" > outfile" \
+				"< infile2 \"l\"\"s\" | \"i\"\"p\"\"\" \"\"\"\"\"-\"\"\"\"s\" \"\"\"l\"\"i\"\"nk\" > outfile" \
 				"< infile2 \a\b\c | \"l\"\"s\"\"\"\ \"\"\"\"\"-\"\"l\"\"a\" > outfile" \
 				"< infile2 \a\b\c | \"l\"\"s\"\"\"\ \"\"\"\"\"-\"\"l\"\"a\" > outfile")
 echo "";
@@ -115,7 +119,36 @@ sizeArray=${#pipexInstructions[@]}
 echo "";
 echo -e "${yellow}PERMORMING TESTS. CHECK THE FOLDER testValgrind TO SEE THE RESULTS${nocolor}";
 echo ""
+flag=0;
 while [ $index -lt $sizeArray ]; do
+	select opt in "${options[@]}";
+	do
+		echo -e "${brightyellow}Between the tests 10 and 17, it is not necessary, in my opinion, to handle these cases exactly as your shell of preference does.\n \
+		However, these tests should never crash your pipex.\n \
+		Press y to continue to these tests or press n to skip these tests and go to test 18 or press q to quit (y/n/q)${nocolor}";
+		read condition1;
+		case $(echo "${condition1}" | tr '[:upper:]' '[:lower:]') in
+		"y")
+			flag=1;
+			break
+			;;
+		"n")
+			flag=2;
+			break
+			;;
+		"q")
+			echo -e "${green}Test for mandatory part done!!${nocolor}";
+			exit 0;
+			;;
+		*)
+			echo -e "${brightred}Option not valid. Try again please.${nocolor}";
+			echo "";
+			;;
+		esac
+	done
+	if [ $flag -eq 2 ]; then
+		break
+	fi
 	echo -e "${yellow}Performing test number $(expr $index + 1).${nocolor}";
 	echo "";
 	{
@@ -131,19 +164,65 @@ while [ $index -lt $sizeArray ]; do
 			echo "";
 			echo "Content of outfile from bash:";
 			cat outfile;
-			rm -f outfile;
 		fi
 		echo "";
 		echo "PIPEX:";
-		eval "${pipexInstructions[$index]}";
-		returnPipex=$?
+		if [ $index -ne 8 ]; then
+			eval "${pipexInstructions[$index]}";
+		else
+			eval "${pipexInstructions[$index]} &";
+		fi
+		if [ $index -eq 3 ] || [ $index -eq 8 ]; then
+			echo -ne "${yellow}Checking the existance of a output file: ${nocolor}";
+			sleep 1;
+			if [ ! -f "outfile2" ]; then
+				echo -e "${red}KO.${nocolor}";
+				echo "";
+			else
+				echo -e "${green}OK.${nocolor}";
+				echo "";
+			fi
+		fi
+		if [ $index -eq 8 ]; then
+			echo -ne "${yellow}Checking the contents of output files generated by shell and pipex: ${nocolor}";
+			diff outfile outfile2;
+			if [ $? -ne 0 ]; then
+				echo -e "${red}KO.${nocolor}";
+				echo "";
+			else
+				echo -e "${green}OK.${nocolor}";
+				echo "";
+			fi
+			sleep 4;
+		fi
+		before=$(date +%s);
 		eval "${pipexInstructions[$index]}" &> temp;
+		returnPipex=$?
+		after=$(date +%s);
+		if [ $index -eq 8 ]; then
+			echo -ne "${yellow}Checking the execution time of pipex in test 9: ${nocolor}";
+			diffTime=$((after - before));
+			if [ $diffTime -lt 1 ] || [ $diffTime -gt 16 ]; then
+				echo -e "${red}KO.${nocolor}";
+			else
+				echo -e "${green}OK.${nocolor}";
+			fi
+		fi
 		cat temp | awk -F ":" '{print $(NF-1), $NF}' > pipex_output;
-		if [ -f "outfile" ]; then
+		if [ -f "outfile2" ]; then
 			echo "";
 			echo "Content of outfile from pipex:";
-			cat outfile;
-			rm -f outfile;
+			cat outfile2;
+		fi
+		echo "";
+		if [ $index -gt 3 ] && [ $index -ne 8 ]; then
+			echo -ne "${yellow}Checking the contents of output files generated by shell and pipex: ";
+			diff outfile outfile2;
+			if [ $? -ne 0 ]; then
+				echo -e "${red}KO.${nocolor}";
+			else
+				echo -e "${green}OK.${nocolor}";
+			fi
 		fi
 		echo "";
 		echo "Return value of bash: ${returnShell}. Return value of pipex: ${returnPipex}";
@@ -154,7 +233,7 @@ while [ $index -lt $sizeArray ]; do
 			echo -e "${green}OK.${nocolor}";
 		fi
 		echo "";
-		if [ $index -ne 6 ]; then
+		if [ $index -ne 9 ]; then
 			echo -ne "${yellow}Checking error output: ${nocolor}";
 			diff shell_output pipex_output;
 			if [ $? -eq 1 ]; then
@@ -162,6 +241,8 @@ while [ $index -lt $sizeArray ]; do
 			else
 				echo -e "${green}OK.${nocolor}";
 			fi
+		else
+			echo -e "${yellow}Test 10: There're some differences in the output gotten in a script execution and executing the command in the shell. Dropping this analysis${nocolor}";
 		fi
 	} 2>&1 | tee test"$(expr $index + 1)".txt;
 	{
@@ -187,8 +268,27 @@ while [ $index -lt $sizeArray ]; do
 	((index++));
 	echo "";
 	echo "";
-	sleep 2;
+	rm -f outfile;
+	rm -f outfile2;
+	select opt2 in "${options[@]}"
+	do
+		echo -e "${brightnocolor}Do you want to continue to the next test? Press y to continue, press n to finish. (y/n).${nocolor}";
+		case $(echo "$opt" | tr '[:upper:]' '[:lower:]') in
+		"y")
+			break
+			;;
+		"n")
+			echo -e "${green}Test for mandatory part done!!${nocolor}"
+			exit 0;
+			;;
+		*)
+			echo -e "${brightred}Option not valid. Try again please.${nocolor}";
+			echo "";
+		esac
+	done
+	echo "";
 done
+
 bash unsetting_bash.sh;
 echo -e "${green}Test for mandatory part done!!${nocolor}"
 make fclean > /dev/null
